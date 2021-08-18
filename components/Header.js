c16d404c8edd2ab50f1acc35e11ca4376ea287b5
Avivit Weissman <wavivit@gmail.com>
2021-08-18 10:44:22 -0700

    add Header and Layout components

---
+++ components/Header.js
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <nav>
+      <div>
+        <h1>Moody - Boards</h1>
+      </div>
+      <a>Home</a>
+      <a>About</a>
+      <a>Explore</a>
+    </nav>
+  );
+};
+
+export default Header;
