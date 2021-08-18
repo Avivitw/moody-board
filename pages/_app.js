29b0c7cc809b0670452fabe0dad73d5d9ea197b5
Avivit Weissman <wavivit@gmail.com>
2021-08-18 10:45:06 -0700

    add the layout to the the whole app

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import { Provider } from "next-auth/client";
-
+import Layout from "../components/Layout";
 import "tailwindcss/tailwind.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Provider session={pageProps.session}>
-      <Component {...pageProps} />
-    </Provider>
+    <Layout>
+      <Provider session={pageProps.session}>
+        <Component {...pageProps} />
+      </Provider>
+    </Layout>
   );
 }
