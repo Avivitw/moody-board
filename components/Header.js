a004d6808d6d766d9138b2beb08b3ba2b75202ff
Avivit Weissman <wavivit@gmail.com>
2021-08-18 10:54:06 -0700

    Add some dumy links to Header component

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,21 @@
+import Link from "next/link";
+
 const Header = () => {
   return (
-    <nav>
+    <header>
       <div>
         <h1>Moody - Boards</h1>
+        <Link href="/">
+          <a>My Boards</a>
+        </Link>
+        <Link href="/">
+          <a>Explore</a>
+        </Link>
+        <Link href="/">
+          <a>User</a>
+        </Link>
       </div>
-      <a>Home</a>
-      <a>About</a>
-      <a>Explore</a>
-    </nav>
+    </header>
   );
 };
 
