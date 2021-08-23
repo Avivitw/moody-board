4c46c3190086ed191687f0984dc003bc074d0a0d
Avivit Weissman <wavivit@gmail.com>
2021-08-23 15:42:16 -0700

    added picture and style

--- pages/select-design.js
+++ pages/select-design.js
@@ -6,9 +6,20 @@ import BoardStyle from "../components/BoardStyle";
 export default function selectDesign() {
   return (
     <>
-      <div class="flex flex-row bg-secondaryA  max-w-screen-lg mx-auto">
-        <h1>Let's start...</h1>
-        <p>First let's pick style that you love</p>
+      <div class="div-board">
+        <h1 class=" text-2xl">Let's start...</h1>
+      </div>
+      <p class="m-4 text-xl max-w-screen-lg mx-auto">
+        First let's pick style that you love
+      </p>
+      <div class="self-center max-w-screen-lg mx-auto">
+        <Image
+          class="max-w-screen-lg mx-auto self-center"
+          src="/minimalist.jpeg"
+          alt="minimalist design"
+          width={700}
+          height={700}
+        />
       </div>
 
       <BoardStyle></BoardStyle>
